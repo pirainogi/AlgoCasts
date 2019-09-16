9950ccbc8922dc21cabc402a00b7d697b4eb5ecd
pirainogi <pirainogi@gmail.com>
2019-09-16 11:24:47 -0400

    reversed string sol 2

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -13,4 +13,20 @@ function reverse(str) {
   return reversedString
 }
 
+//cleaned up version of above
+// function reverse(str) {
+//   return str.split("").reverse().join("")
+// }
+
+//create an empty string
+//take the last char of the orig string and add to the start of the new string
+//return the reversed string
+// function reverse(str) {
+//   let reversed = ''
+//   for (let char of str){
+//     reversed = char + reversed
+//   }
+//   return reversed
+// }
+
 module.exports = reverse;
