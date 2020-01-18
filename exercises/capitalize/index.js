8db810f3a31c7ba34f6c6c2cf93f61d5df56ce10
pirainogi <pirainogi@gmail.com>
2020-01-18 12:08:53 -0500

    capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,34 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize1(str) {
+  //split the string on any spaces
+  //capitalize the first character in every element in the array
+  //join the array with a space between elements
+  //return the new string
+  let arr = []
+  for(const word of str.split(" ")){
+    arr.push(word[0].toUpperCase() + word.slice(1))
+  }
+  return arr.join(" ")
+}
+
+function capitalize2(str){
+  //create an empty string
+  //iterate through the string, if any character to the left of current char
+  //capitalize and add to result
+  //otherwise just add to result string
+  //how to handle the first character? nothing to the left > automatically capitalize the first char
+
+  let result = str[0].toUpperCase()
+  for(let i = 1; i < str.length; i++){
+    if(str[i-1] === " "){
+      result += str[i].toUpperCase()
+    } else {
+      result += str[i]
+    }
+  }
+  return result
+}
 
 module.exports = capitalize;
