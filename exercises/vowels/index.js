43ed911157e58cb7ecd897256c102ffcc5f7651c
pirainogi <pirainogi@gmail.com>
2020-01-18 17:43:40 -0500

    vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,20 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  let counter = 0
+  const checker = ['a', 'e', 'i', 'o', 'u']
+  for(let char of str.toLowerCase()){
+    if(checker.includes(char)){
+      counter++
+    }
+  }
+  return counter
+}
+
+function vowels2(str){
+  const matches = str.match(/[aeiou]/gi)
+  return matches ? matches.length : 0
+}
 
 module.exports = vowels;
